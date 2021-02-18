2b4005e6542c33569d6b4faa6edbb0adaeca6dba
Kailey <kalyhart@iu.edu>
2021-02-18 08:56:59 -0500

    Added sign up

---
+++ admin/register_view.php
@@ -0,0 +1,7 @@
+
+<form name="signup" action="../actions/signup.php" method="POST"> 
+    <input type="text" name="username" placeholder="Username" required/>
+    <input type="text" name="password" placeholder="Password" required/>
+    <input type="submit" name="submit" value="Register" />
+
+</form>
