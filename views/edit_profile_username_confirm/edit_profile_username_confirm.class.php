8b92233404b24d5a21d22550d93d152c4580ab87
Kailey <kalyhart@iu.edu>
2021-04-08 17:16:14 -0400

    Fixed debug comment showing on edit_profile_username_confirm

--- views/edit_profile_username_confirm/edit_profile_username_confirm.class.php
+++ views/edit_profile_username_confirm/edit_profile_username_confirm.class.php
@@ -17,7 +17,7 @@ class EditProfileUsernameConfirm extends MainView
             <a href="index.php?action=register"><button class="btn btn-dark mt-5">Try Again</button></a>';
         }
 
-        print_r($result);
+        
 ?>
         <div style="max-width: 900px; display: flex;flex-direction: column;   margin: 0 auto; text-align: center;">
                 <h2 style="margin-bottom: 30px; margin-top: 30px; ">You successfully edited your username!</h2>
